58c00fd1d2cf27cfd18a2947b329a024fef4dece
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-13 12:07:25 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    double a=0, b=0, c=0, resultadocommais=0, resultadocommenos=0, delta=0;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    delta = (b * b) - ( 4 * a * c );
+    if (delta < 0 || a == 0){
+        cout << "Impossivel calcular" << endl;
+    }else{
+        resultadocommais = ( ( b * -1 ) + sqrt(delta) ) / (2 * a);
+        resultadocommenos = ( ( b * -1 ) - sqrt(delta) ) / (2 * a);
+        cout.precision(5);
+        cout << fixed << "R1 = " << resultadocommais << endl;
+        cout << fixed << "R2 = " << resultadocommenos << endl;
+    }
+    return 0;
+}
